c8afad06cd11085893877304c4baba2d2e8fa839
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-12 17:27:25 +0500

    Layout is created along with the grid and the design is ready for he grid

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -12,11 +12,47 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var pets: [Pet]
     
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                EmptyView()
+                LazyVGrid(columns: layout) {
+                    GridRow {
+                        ForEach(pets) { pet in
+                            NavigationLink(destination: EmptyView()) {
+                                VStack {
+                                    
+                                    if let imageData = pet.photo {
+                                        if let image = UIImage(data: imageData) {
+                                            Image(uiImage: image)
+                                        }
+                                    } else {
+                                        
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    
+                                    Spacer()
+                                } //: VSTACK
+                                .frame(minWidth: 0,maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            } //: NAVLINK
+                            .foregroundStyle(.primary)
+                        } //: LOOP
+                    } //: GRID ROW
+                } //: GRID LAYOUT
+                .padding(.horizontal)
             } //: SCROLLVIEW
+            .navigationTitle(pets.isEmpty ? "" : "PAWS")
             .overlay {
                 if pets.isEmpty {
                     CustomContentUnavailableView(

e5e803c10c96130b0cc3b9032734a3f854fab7c7
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-12 16:39:47 +0500

    Swift data is setup in the conetnt view main app

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query private var pets: [Pet]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,6 +23,7 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("No Data") {
     ContentView()
+        .modelContainer(for: Pet.self, inMemory: true)
 }

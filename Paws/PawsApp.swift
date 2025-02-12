fffed843c03cc191c5ac662245aafac616389a06
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-12 15:55:15 +0500

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Elexoft on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

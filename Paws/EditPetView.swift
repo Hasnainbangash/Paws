8dc603d2f6770c37656b1c2dbe6d44d1ae951059
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-13 10:45:26 +0500

    Edit pet view sample data is reader

--- Paws/EditPetView.swift
+++ Paws/EditPetView.swift
@@ -9,19 +9,25 @@ import SwiftUI
 import SwiftData
 
 struct EditPetView: View {
+    // We use this property binding to create bindings to mutable properties of a data modal objects that conforms to the observable protocol.
+    @Bindable var pet: Pet
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    do {
-        let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
-        let container = try ModelContainer(for: Pet.self, configurations: configuration)
-        let sampleData = Pet(name: "Daisy")
-        return EditPetView()
-            .modelContainer(container)
-    } catch {
-        fatalError("Could not load preview data. \(error.localizedDescription)")
+    NavigationStack {
+        do {
+            let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+            let container = try ModelContainer(for: Pet.self, configurations: configuration)
+            let sampleData = Pet(name: "Daisy")
+            
+            return EditPetView(pet: sampleData)
+                .modelContainer(container)
+        } catch {
+            fatalError("Could not load preview data. \(error.localizedDescription)")
+        }
     }
 }
